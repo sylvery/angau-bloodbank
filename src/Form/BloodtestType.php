70b6e6e65f9f27115df3f00c6558746b47b70bd9
sylvery <silvery4543@gmail.com>
2020-06-24 06:40:32 +1000

    blood test ui

--- src/Form/BloodtestType.php
+++ src/Form/BloodtestType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Bloodbag;
 use App\Entity\Bloodtest;
+use App\Entity\Sicktype;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +17,22 @@ class BloodtestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
             ->add('comment')
+            ->add('sickfound', EntityType::class, [
+                'class' => Sicktype::class,
+                'multiple' => true,
+                'expanded' => true,
+                'choice_attr' => ['class' => 'input-group'],
+                'placeholder' => 'Select type sickness',
+            ])
+            ->add('bloodbag', EntityType::class, [
+                'class' => Bloodbag::class,
+                'placeholder' => 'Select bloodbags tested',
+            ])
+            ->add('submit', SubmitType::class)
         ;
     }
 
